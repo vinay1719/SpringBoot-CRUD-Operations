465faaf9064800eb3d089f3ab0f59631acc86a99
Vinay Acharya <85022594+vinay1719@users.noreply.github.com>
2024-07-30 13:04:51 +0530

    Delete SpringBoot-Crud-Operation/src/main/java/com/SpringBootCrud/SpringBoot/Crud/Operation/repository/EmployeeRepository.java

--- SpringBoot-Crud-Operation/src/main/java/com/SpringBootCrud/SpringBoot/Crud/Operation/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.SpringBootCrud.SpringBoot.Crud.Operation.repository;
-
-import com.SpringBootCrud.SpringBoot.Crud.Operation.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-}
