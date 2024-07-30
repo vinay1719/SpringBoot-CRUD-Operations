50822bede68d0ee6a9a4d3335015f19abea0f812
Vinay Acharya <85022594+vinay1719@users.noreply.github.com>
2024-07-30 12:48:51 +0530

    Create HomeController.java

---
+++ SpringBoot-Crud-Operation/src/main/java/com/SpringBootCrud/SpringBoot/Crud/Operation/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.SpringBootCrud.SpringBoot.Crud.Operation.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+}
