a5a3ecbe515f02161a85c69e5047e1e97ea71797
Vinay Acharya <85022594+vinay1719@users.noreply.github.com>
2024-07-30 13:05:15 +0530

    Create UserService.java

---
+++ SpringBoot-Crud-Operation/src/main/java/com/SpringBootCrud/SpringBoot/Crud/Operation/service/UserService.java
@@ -0,0 +1,30 @@
+package com.SpringBootCrud.SpringBoot.Crud.Operation.service;
+
+import com.SpringBootCrud.SpringBoot.Crud.Operation.entity.User;
+import com.SpringBootCrud.SpringBoot.Crud.Operation.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User getUserById(Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
+}
