f73b7838629a0202225caab492fe5bc5cfca5d72
Vinay Acharya <85022594+vinay1719@users.noreply.github.com>
2024-07-30 13:05:55 +0530

    Create UserController.java

---
+++ SpringBoot-Crud-Operation/src/main/java/com/SpringBootCrud/SpringBoot/Crud/Operation/controller/UserController.java
@@ -0,0 +1,56 @@
+package com.SpringBootCrud.SpringBoot.Crud.Operation.controller;
+
+import com.SpringBootCrud.SpringBoot.Crud.Operation.entity.User;
+import com.SpringBootCrud.SpringBoot.Crud.Operation.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/users")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public String getAllUsers(Model model) {
+        List<User> users = userService.getAllUsers();
+        model.addAttribute("users", users);
+        return "index"; // Return the index.html view
+    }
+
+    @GetMapping("/new")
+    public String createUserForm(Model model) {
+        model.addAttribute("user", new User());
+        return "user-form"; // Create a user form template
+    }
+
+    @PostMapping
+    public String saveUser(@ModelAttribute User user) {
+        userService.saveUser(user);
+        return "redirect:/users";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String editUserForm(@PathVariable Long id, Model model) {
+        User user = userService.getUserById(id);
+        model.addAttribute("user", user);
+        return "user-form"; // Edit user form template
+    }
+
+    @PostMapping("/update/{id}")
+    public String updateUser(@PathVariable Long id, @ModelAttribute User user) {
+        user.setId(id);
+        userService.saveUser(user);
+        return "redirect:/users";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+        return "redirect:/users";
+    }
+}
