bc964e6ced0b38ca65db2d4ba9d7330fff2537df
Vinay Acharya <85022594+vinay1719@users.noreply.github.com>
2024-07-30 12:51:06 +0530

    Create EmployeeService.java

---
+++ SpringBoot-Crud-Operation/src/main/java/com/SpringBootCrud/SpringBoot/Crud/Operation/service/EmployeeService.java
@@ -0,0 +1,42 @@
+package com.SpringBootCrud.SpringBoot.Crud.Operation.service;
+
+import com.SpringBootCrud.SpringBoot.Crud.Operation.entity.Employee;
+import com.SpringBootCrud.SpringBoot.Crud.Operation.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EmployeeService {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    public Employee getEmployeeById(Long id) {
+        return employeeRepository.findById(id).orElse(null);
+    }
+
+    public Employee saveEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    public Employee updateEmployee(Long id, Employee updatedEmployee) {
+        Employee employee = employeeRepository.findById(id).orElse(null);
+        if (employee != null) {
+            employee.setName(updatedEmployee.getName());
+            employee.setEmail(updatedEmployee.getEmail());
+            employee.setPhoneNumber(updatedEmployee.getPhoneNumber());
+            return employeeRepository.save(employee);
+        }
+        return null;
+    }
+
+    public void deleteEmployee(Long id) {
+        employeeRepository.deleteById(id);
+    }
+}
