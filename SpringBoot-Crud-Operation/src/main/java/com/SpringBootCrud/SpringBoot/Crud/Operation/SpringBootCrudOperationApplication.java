744a8e88533d60dd8f12524fd87c7d5e2b24a0f8
Vinay Acharya <85022594+vinay1719@users.noreply.github.com>
2024-07-30 13:01:09 +0530

    Update SpringBootCrudOperationApplication.java

--- SpringBoot-Crud-Operation/src/main/java/com/SpringBootCrud/SpringBoot/Crud/Operation/SpringBootCrudOperationApplication.java
+++ SpringBoot-Crud-Operation/src/main/java/com/SpringBootCrud/SpringBoot/Crud/Operation/SpringBootCrudOperationApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootCrudOperationApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootCrudOperationApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCrudOperationApplication.class, args);
+    }
 }
